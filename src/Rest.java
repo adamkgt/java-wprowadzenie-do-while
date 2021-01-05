c9cf925568fb629c86f53dbba4b51e827411efee
Adam Kogut <adamkgt@gmail.com>
2021-01-05 18:41:37 +0100

    #2 Zadanie2.

---
+++ src/Rest.java
@@ -0,0 +1,16 @@
+public class Rest {
+    public static void main(String[] args) {
+
+        int n = 7;
+
+        do {
+            System.out.println(n % 2);
+
+            n = n/2;
+
+
+        }while(n>0);
+
+    }
+}
+
